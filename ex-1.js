4aa48efc48fc5def2fc86b9e07d43b979e9be28e
Krit <titatit.akkh@gmail.com>
2023-11-27 10:03:53 +0700

    feat: Finish Ex.1-3

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,15 @@
 // Exercise #1: Food Order
 
 // Start coding here
+const foodOrder = Object.freeze({
+    orderNumber: "A0234",
+    address: "Bangkok",
+    paymentType: "Cash",
+    totalPrice: 4500,
+    restaurentName: "MK"
+});
+
+foodOrder.totalPrice = 5000;
+foodOrder.paymentType = "Credit Card";
+
+console.log(foodOrder);
